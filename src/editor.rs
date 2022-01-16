64d3bbf058598f63b85382e786e4516832e33698
yuucu <yuucu.sub@gmail.com>
2022-01-17 01:16:41 +0900

    chapter 3

--- src/editor.rs
+++ src/editor.rs
@@ -1,53 +1,123 @@
-use std::io::{self, stdout};
+use crate::Terminal;
+use std::io::Write;
 use termion::event::Key;
-use termion::input::TermRead;
-use termion::raw::IntoRawMode;
 
-pub struct Editor {}
+const VERSION: &str = env!("CARGO_PKG_VERSION");
 
-fn die(e: std::io::Error) {
-    panic!("error: {}", e);
+pub struct Position {
+    pub x: usize,
+    pub y: usize,
+}
+
+pub struct Editor {
+    should_quit: bool,
+    terminal: Terminal,
+    cursor_position: Position,
 }
 
 impl Editor {
-    pub fn run(&self) {
-        let _stdout = stdout().into_raw_mode().unwrap();
-        for key in io::stdin().keys() {
-            match key {
-                Ok(key) => match key {
-                    Key::Char(c) => {
-                        if c.is_control() {
-                            println!("{:?} \r", c as u8);
-                        } else {
-                            println!("{:?} ({})\r", c as u8, c);
-                        }
-                    }
-                    Key::Ctrl('q') => break,
-                    _ => println!("{:?} \r", key),
-                },
-                Err(err) => die(err),
+    pub fn run(&mut self) {
+        loop {
+            if let Err(error) = self.refresh_screen() {
+                die(error);
+            }
+            if self.should_quit {
+                break;
+            }
+            if let Err(error) = self.process_keypress() {
+                die(error);
             }
         }
     }
 
     pub fn default() -> Self {
-        Self {}
+        Self {
+            should_quit: false,
+            terminal: Terminal::default().expect(""),
+            cursor_position: Position { x: 0, y: 0 },
+        }
     }
 
-    fn process_keypress(&self) -> Result<(), std::io::Error> {
-        let pressed_key = read_key()?;
+    fn refresh_screen(&self) -> Result<(), std::io::Error> {
+        Terminal::cursor_position(&Position { x: 0, y: 0 });
+        print!("{}{}", termion::clear::All, termion::cursor::Goto(1, 1));
+        if self.should_quit {
+            Terminal::clear_screen();
+            println!("Goodbye.\r");
+        } else {
+            self.draw_rows();
+            Terminal::cursor_position(&self.cursor_position);
+        }
+        Terminal::flush()
+    }
+
+    fn process_keypress(&mut self) -> Result<(), std::io::Error> {
+        let pressed_key = Terminal::read_key()?;
         match pressed_key {
-            Key::Ctrl('q') => panic!("Program end"),
-            _ => (),
+            Key::Ctrl('q') => self.should_quit = true,
+            Key::Char('h')
+            | Key::Char('j')
+            | Key::Char('k')
+            | Key::Char('l')
+            | Key::PageUp
+            | Key::PageDown
+            | Key::End
+            | Key::Home => self.move_cursor(pressed_key),
+            _ => println!("{:?} \r", pressed_key),
         }
         Ok(())
     }
-}
+    fn move_cursor(&mut self, key: Key) {
+        let Position { mut y, mut x } = self.cursor_position;
+        let size = self.terminal.size();
+        let height = size.height.saturating_sub(1) as usize;
+        let width = size.width.saturating_sub(1) as usize;
+        match key {
+            Key::Char('k') => y = y.saturating_sub(1),
+            Key::Char('j') => {
+                if y < height {
+                    y = y.saturating_add(1);
+                }
+            }
+            Key::Char('h') => x = x.saturating_sub(1),
+            Key::Char('l') => {
+                if x < width {
+                    x = x.saturating_add(1)
+                }
+            }
+            Key::PageUp => y = 0,
+            Key::PageDown => y = height,
+            Key::Home => x = 0,
+            Key::End => x = width,
 
-fn read_key() -> Result<Key, std::io::Error> {
-    loop {
-        if let Some(key) = io::stdin().lock.keys().next() {
-            return key;
+            _ => (),
+        }
+        self.cursor_position = Position { x, y }
+    }
+    fn draw_welcome_message(&self) {
+        let mut welcome_message = format!("Hecto editor -- version {}\r", VERSION);
+        let len = welcome_message.len();
+        let width = self.terminal.size().width as usize;
+        let padding = width.saturating_sub(len) / 2;
+        let spaces = " ".repeat(padding.saturating_sub(1));
+        welcome_message = format!("~{}{}", spaces, welcome_message);
+        welcome_message.truncate(width);
+        println!("{}\r", &welcome_message)
+    }
+    fn draw_rows(&self) {
+        let height = self.terminal.size().height;
+        Terminal::clear_current_line();
+        for row in 0..height - 1 {
+            if row == height / 3 {
+                self.draw_welcome_message()
+            } else {
+                println!("~\r");
+            }
         }
     }
+}
+
+fn die(e: std::io::Error) {
+    Terminal::clear_screen();
+    panic!("error: {}", e);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 #![warn(clippy::all, clippy::pedantic)]
 mod editor;
+mod terminal;
+
 use editor::Editor;
+pub use editor::Position;
+pub use terminal::Terminal;
 
 fn main() {
-    let editor = Editor::default();
-    editor.run();
+    Editor::default().run();
 }
